50fe4c090b9b7dcea758575bbe0bfe1285882796
zhangxiancai <33056478+zhangxiancai@users.noreply.github.com>
2022-09-29 23:37:07 +0800

    Rename S11_demo.m to VCRF_demo.m

--- VCRF_demo.m
+++ VCRF_demo.m
@@ -1,43 +1,43 @@
 % demo
 clear all
-% train_rfmodel();%ÑµÁ·Ëæ»úÉ­ÁÖÄ£ĞÍ²¢±£´æ
-original_data=csvread('D:\TD_DATA\2021-1-15_data\20210115_103952_ÈıÁ¾_Æû³µ.txt');%ÔØÈë´ı¼ì²âÔ­Ê¼Êı¾İ
-se_pairs=vehicle_detection(original_data,17,1.2);%³µÁ¾¼ì²â
-[~,numb]=size(se_pairs);%³µÁ¾¸öÊı
+% train_rfmodel();%è®­ç»ƒéšæœºæ£®æ—æ¨¡å‹å¹¶ä¿å­˜
+original_data=csvread('D:\TD_DATA\2021-1-15_data\20210115_103952_ä¸‰è¾†_æ±½è½¦.txt');%è½½å…¥å¾…æ£€æµ‹åŸå§‹æ•°æ®
+se_pairs=vehicle_detection(original_data,17,1.2);%è½¦è¾†æ£€æµ‹
+[~,numb]=size(se_pairs);%è½¦è¾†ä¸ªæ•°
 types=zeros(1,numb);
 for i=1:numb
-    types(i)=vehicle_classification(original_data(se_pairs(1,i):se_pairs(2,i),2:end));%³µÁ¾·ÖÀà
+    types(i)=vehicle_classification(original_data(se_pairs(1,i):se_pairs(2,i),2:end));%è½¦è¾†åˆ†ç±»
 end
-display(original_data,se_pairs,types);%»­³µÁ¾¼ì²âºÍ³µÁ¾·ÖÀà½á¹ûÍ¼
+display(original_data,se_pairs,types);%ç”»è½¦è¾†æ£€æµ‹å’Œè½¦è¾†åˆ†ç±»ç»“æœå›¾
 
 
 function se_pairs=vehicle_detection(original_data,l_g,w_thre)
-%³µÁ¾¼ì²â
-%ÊäÈë£ºoriginal_data=Ô­Ê¼Êı¾İ ~*827£¬l_g=½á¹¹Ìå³¤¶È£¬ w_thre=¶¯Ì¬ãĞÖµµÄÈ¨Öµ
-%Êä³ö£ºÆğÖ¹Ê±¼ä¶Ô 2*~
+%è½¦è¾†æ£€æµ‹
+%è¾“å…¥ï¼šoriginal_data=åŸå§‹æ•°æ® ~*827ï¼Œl_g=ç»“æ„ä½“é•¿åº¦ï¼Œ w_thre=åŠ¨æ€é˜ˆå€¼çš„æƒå€¼
+%è¾“å‡ºï¼šèµ·æ­¢æ—¶é—´å¯¹ 2*~
 [r_n,~]=size(original_data);
 r_mean=zeros(1,r_n);
 S=zeros(1,r_n);
 %
-%¶¯Ì¬ãĞÖµ
+%åŠ¨æ€é˜ˆå€¼
 baseline=106;
 for i=1:r_n
-    r_mean(i)=mean(original_data(i,2:end));%¾ùÖµ
-    thre=baseline*w_thre;%¶¯Ì¬ãĞÖµ
+    r_mean(i)=mean(original_data(i,2:end));%å‡å€¼
+    thre=baseline*w_thre;%åŠ¨æ€é˜ˆå€¼
     if r_mean(i)>thre
         S(i)=1;%
     else
-        baseline=0.8*baseline+0.2*r_mean(i);%¸üĞÂ»ù×¼Ïß
+        baseline=0.8*baseline+0.2*r_mean(i);%æ›´æ–°åŸºå‡†çº¿
     end  
 end
 %
-S_filt=my_dilate(my_erode(my_erode(my_dilate(S,l_g),l_g),l_g),l_g);%±Õ¿ªÔËËã
-S_filter_diff=[S_filt,0]-[0,S_filt];%²î·Ö
+S_filt=my_dilate(my_erode(my_erode(my_dilate(S,l_g),l_g),l_g),l_g);%é—­å¼€è¿ç®—
+S_filter_diff=[S_filt,0]-[0,S_filt];%å·®åˆ†
 % figure;plot(500*S);hold on;plot(500*S_filt+600);hold on;plot(r_mean);
 % figure;plot(S_filter_diff);ylim([-2,2]);
 
-%Í³¼ÆÆğÖ¹Ê±¼ä
-numb=length(find(S_filter_diff==-1));%³µÁ¾Ñù±¾¸öÊı
+%ç»Ÿè®¡èµ·æ­¢æ—¶é—´
+numb=length(find(S_filter_diff==-1));%è½¦è¾†æ ·æœ¬ä¸ªæ•°
 se_pairs=zeros(2,numb);
 j=0;
 for i=1:r_n+1
@@ -55,56 +55,56 @@ end
 end
 
 function type=vehicle_classification(X)
-%³µÁ¾·ÖÀà
-%ÊäÈë£ºÔ­Ê¼³µÁ¾Ñù±¾ ~*827
-%Êä³ö£ºÀà±ğ
-m=16;%Ñù±¾¿í¶È
+%è½¦è¾†åˆ†ç±»
+%è¾“å…¥ï¼šåŸå§‹è½¦è¾†æ ·æœ¬ ~*827
+%è¾“å‡ºï¼šç±»åˆ«
+m=16;%æ ·æœ¬å®½åº¦
 crests=zeros(1,m*6);
 
-resize_X=imresize(X,[m,827],'bilinear');%µ÷Õû´óĞ¡
+resize_X=imresize(X,[m,827],'bilinear');%è°ƒæ•´å¤§å°
 for j=1:m
-    crests(6*j-6+1:6*j)=get_crests(resize_X(j,:));%¼ÆËã²¨·å
+    crests(6*j-6+1:6*j)=get_crests(resize_X(j,:));%è®¡ç®—æ³¢å³°
 end
-vehicle_height=get_vehicle_height(resize_X);%¼ÆËãµ×ÅÌ¸ß¶È
+vehicle_height=get_vehicle_height(resize_X);%è®¡ç®—åº•ç›˜é«˜åº¦
 
 load rf_model.mat %load the trained random forest model
 type=predict(rf_model,[crests,vehicle_height]);
 end
 
 function vehicle_height=get_vehicle_height(vehicle_sample)
-%ÊäÈë: ³µÁ¾Ñù±¾  16*827 
-%Êä³ö: µ×ÅÌ¸ß¶È
+%è¾“å…¥: è½¦è¾†æ ·æœ¬  16*827 
+%è¾“å‡º: åº•ç›˜é«˜åº¦
 [n,~]=size(vehicle_sample);
 hs=zeros(1,n);
 for i=1:n
     crests=get_crests(vehicle_sample(i,:));
     hs(i)=crests(1)*0.48+200;% mm
 end
-vehicle_height=mean(hs);%µ×ÅÌ¸ß¶È
+vehicle_height=mean(hs);%åº•ç›˜é«˜åº¦
 
 end
 
 
 function crests_sorted=get_crests(signal)
-%¼ÆËã²¨·å
-%ÊäÈë£ºa Envelope data 1*827
-%Êä³ö£º²¨·å 1*6 £¨°´Õñ·ù´óĞ¡ÅÅĞò£©
-temp=medfilt(medianfilt(signal,26),13);%ÖĞÖµ¾ùÖµÂË²¨
+%è®¡ç®—æ³¢å³°
+%è¾“å…¥ï¼ša Envelope data 1*827
+%è¾“å‡ºï¼šæ³¢å³° 1*6 ï¼ˆæŒ‰æŒ¯å¹…å¤§å°æ’åºï¼‰
+temp=medfilt(medianfilt(signal,26),13);%ä¸­å€¼å‡å€¼æ»¤æ³¢
 [~,temp_s]=size(temp);
-candidates=zeros(100,2);%ºòÑ¡µã
+candidates=zeros(100,2);%å€™é€‰ç‚¹
 z=1;
 for j=11:temp_s-10
     if any([temp(j)-temp(j-1)>0,temp(j)-temp(j-2)>0,temp(j)-temp(j-3)>0,temp(j)-temp(j-4)>0,temp(j)-temp(j-5)>0,temp(j)-temp(j-6)>0,temp(j)-temp(j-7)>0,temp(j)-temp(j-8)>0,temp(j)-temp(j-9)>0,temp(j)-temp(j-10)>0]) ...
             &&any([temp(j)-temp(j+1)>0,temp(j)-temp(j+2)>0,temp(j)-temp(j+3)>0,temp(j)-temp(j+4)>0,temp(j)-temp(j+5)>0,temp(j)-temp(j+6)>0,temp(j)-temp(j+7)>0,temp(j)-temp(j+8)>0,temp(j)-temp(j+9)>0,temp(j)-temp(j+10)>0])
         
-        candidates(z,1)=j;%ºòÑ¡µãÏÂ±ê
-        candidates(z,2)=temp(j);%ºòÑ¡µãÕñ·ù
+        candidates(z,1)=j;%å€™é€‰ç‚¹ä¸‹æ ‡
+        candidates(z,2)=temp(j);%å€™é€‰ç‚¹æŒ¯å¹…
         z=z+1;
     end
 end
-crests=zeros(10,2);%²¨·å
-reg_index=1;%µ±Ç°ÇøÓòĞòºÅ
-reg_length=1;%µ±Ç°ÇøÓò³¤¶È
+crests=zeros(10,2);%æ³¢å³°
+reg_index=1;%å½“å‰åŒºåŸŸåºå·
+reg_length=1;%å½“å‰åŒºåŸŸé•¿åº¦
 for k=2:100
     if candidates(k,1)==0
         break
@@ -114,29 +114,29 @@ for k=2:100
     end
     if candidates(k,1)==candidates(k-1,1)+1||candidates(k,1)==candidates(k-1,1)+2
         reg_length=reg_length+1;
-        crests(reg_index,1)=candidates(k-floor(reg_length/2),1);%ÇøÓòÖĞĞÄ
-        crests(reg_index,2)=candidates(k-floor(reg_length/2),2);%Õñ·ù
+        crests(reg_index,1)=candidates(k-floor(reg_length/2),1);%åŒºåŸŸä¸­å¿ƒ
+        crests(reg_index,2)=candidates(k-floor(reg_length/2),2);%æŒ¯å¹…
     else
-        if reg_length>1%Èç¹ûÉÏÒ»¸öÇøÓò³¤¶È´óÓÚ1
-            reg_index=reg_index+1;%ÇøÓòĞòºÅ
+        if reg_length>1%å¦‚æœä¸Šä¸€ä¸ªåŒºåŸŸé•¿åº¦å¤§äº1
+            reg_index=reg_index+1;%åŒºåŸŸåºå·
         end
-        reg_length=1;%ÇøÓò³¤¶ÈÎª1
+        reg_length=1;%åŒºåŸŸé•¿åº¦ä¸º1
     end
 end
 % di_candidates_index=candidates(:,1)-[0,candidates(1:end-1,1)];
 %debug
 % figure;plot(temp);hold on;plot(comp_temp_1(:,1),comp_temp_1(:,2),'*');hold on; plot(comp_temp_2(:,1),comp_temp_2(:,2),'+');
 
-%%°´Õñ·ù´óĞ¡ÅÅĞò
+%%æŒ‰æŒ¯å¹…å¤§å°æ’åº
 crests_sorted=zeros(1,6);
 [~,com_index]=sort(crests(:,2),'descend');
-crests_sorted(1:3)=crests(com_index(1:3),1);%ÏÂ±ê
-crests_sorted(4:6)=crests(com_index(1:3),2);%Õñ·ù
+crests_sorted(1:3)=crests(com_index(1:3),1);%ä¸‹æ ‡
+crests_sorted(4:6)=crests(com_index(1:3),2);%æŒ¯å¹…
 end
 
 function X_filt=my_dilate(X,l)
-%Ò»Î¬ÅòÕÍº¯Êı
-%ÊäÈë£ºX=ĞĞÏòÁ¿  l=½á¹¹Ìå³¤¶È
+%ä¸€ç»´è†¨èƒ€å‡½æ•°
+%è¾“å…¥ï¼šX=è¡Œå‘é‡  l=ç»“æ„ä½“é•¿åº¦
 half_l=floor(l/2);
 [~,s]=size(X);
 X_filt=X;
@@ -152,8 +152,8 @@ end
 end
 
 function X_filt=my_erode(X,l)
-%Ò»Î¬¸¯Ê´º¯Êı
-%ÊäÈë£ºX=ĞĞÏòÁ¿  l=½á¹¹Ìå³¤¶È
+%ä¸€ç»´è…èš€å‡½æ•°
+%è¾“å…¥ï¼šX=è¡Œå‘é‡  l=ç»“æ„ä½“é•¿åº¦
 half_l=floor(l/2);
 [~,s]=size(X);
 X_filt=X;
@@ -169,7 +169,7 @@ end
 end
 
 function X_filt=medianfilt(X,l)
-%ÖĞÖµÂË²¨
+%ä¸­å€¼æ»¤æ³¢
 half_l=floor(l/2);
 [~,s]=size(X);
 X_filt=X;
@@ -185,7 +185,7 @@ end
 end
 
 function X_filt=medfilt(X,l)
-%¾ùÖµÂË²¨
+%å‡å€¼æ»¤æ³¢
 half_l=floor(l/2);
 [~,s]=size(X);
 X_filt=X;
@@ -201,24 +201,24 @@ end
 end
 
 function display(original_data,se_pairs,types)
-%»­³µÁ¾¼ì²âºÍ³µÁ¾·ÖÀà½á¹ûÍ¼
-%ÊäÈë£ºÔ­Ê¼Êı¾İ£¬ÆğÖ¹Ê±¼ä¶Ô£¬³µĞÍ
+%ç”»è½¦è¾†æ£€æµ‹å’Œè½¦è¾†åˆ†ç±»ç»“æœå›¾
+%è¾“å…¥ï¼šåŸå§‹æ•°æ®ï¼Œèµ·æ­¢æ—¶é—´å¯¹ï¼Œè½¦å‹
 [~,numb]=size(se_pairs);
 radar_mean=mean(original_data(:,2:end)');
 
 figure
 subplot(2,1,1);
-% mesh(original_data(:,2:end)');title('Ô­Ê¼Êı¾İ');xlabel('Measurement Counts');ylabel('Sample Counts');zlabel('Amplitude');
+% mesh(original_data(:,2:end)');title('åŸå§‹æ•°æ®');xlabel('Measurement Counts');ylabel('Sample Counts');zlabel('Amplitude');
 subplot(2,1,2);
-plot(radar_mean);title('Æ½¾ùÊı¾İ');xlabel('Measurement Counts');ylabel('Averaged Amplitude');
+plot(radar_mean);title('å¹³å‡æ•°æ®');xlabel('Measurement Counts');ylabel('Averaged Amplitude');
 
 figure
 subplot(2,1,1);
-plot(radar_mean);title('³µÁ¾¼ì²â½á¹û');xlabel('Measurement Counts');ylabel('Averaged Amplitude');
+plot(radar_mean);title('è½¦è¾†æ£€æµ‹ç»“æœ');xlabel('Measurement Counts');ylabel('Averaged Amplitude');
 hold on;plot(se_pairs(1,:),radar_mean(se_pairs(1,:)),'s', 'MarkerSize',10,'MarkerEdgeColor','g','MarkerFaceColor','g');
 hold on; plot(se_pairs(2,:),radar_mean(se_pairs(2,:)),'p', 'MarkerSize',10,'MarkerEdgeColor','b','MarkerFaceColor','b');
 subplot(2,1,2);
-plot(radar_mean);title('³µÁ¾·ÖÀà½á¹û');xlabel('Measurement Counts');ylabel('Averaged Amplitude');
+plot(radar_mean);title('è½¦è¾†åˆ†ç±»ç»“æœ');xlabel('Measurement Counts');ylabel('Averaged Amplitude');
 % hold on; plot(se_pairs(1,:),radar_mean(se_pairs(1,:)),'s', 'MarkerSize',10,'MarkerEdgeColor','g','MarkerFaceColor','g');
 % hold on; plot(se_pairs(2,:),radar_mean(se_pairs(2,:)),'p', 'MarkerSize',10,'MarkerEdgeColor','b','MarkerFaceColor','b');
 types_name={'Car' 'SUV' 'Bus' 'Middle-truck'};
@@ -230,51 +230,51 @@ end
 end
 
 function train_rfmodel()
-%ÑµÁ·Ëæ»úÉ­ÁÖÄ£ĞÍ²¢±£´æ
-[X,Y]=load_data();%ÔØÈë³µÁ¾Êı¾İ¼¯
-X_F=data_feature_extract(X);%ÌØÕ÷Êı¾İ¼¯
-md1=fitcensemble(X_F,Y,'Learners','tree','Method','Bag');%ÑµÁ·
+%è®­ç»ƒéšæœºæ£®æ—æ¨¡å‹å¹¶ä¿å­˜
+[X,Y]=load_data();%è½½å…¥è½¦è¾†æ•°æ®é›†
+X_F=data_feature_extract(X);%ç‰¹å¾æ•°æ®é›†
+md1=fitcensemble(X_F,Y,'Learners','tree','Method','Bag');%è®­ç»ƒ
 rf_model = compact(md1);
-save('rf_model.mat','rf_model');%±£´æÄ£ĞÍ
+save('rf_model.mat','rf_model');%ä¿å­˜æ¨¡å‹
 end
 
 function X_F=data_feature_extract(X)
-%¼ÆËãÌØÕ÷¼¯
-%ÊäÈë£º³µÁ¾Êı¾İ¼¯ cell(1,~)
-%Êä³ö£ºÌØÕ÷¼¯ ~*m*6+1
+%è®¡ç®—ç‰¹å¾é›†
+%è¾“å…¥ï¼šè½¦è¾†æ•°æ®é›† cell(1,~)
+%è¾“å‡ºï¼šç‰¹å¾é›† ~*m*6+1
 m=16;
 [~,samples_count]=size(X);
 X_F=zeros(samples_count,m*6+1);
 crests=zeros(1,m*6);
 for i=1:samples_count% 
-    resize_X=imresize(X{i},[m,827],'bilinear');%µ÷Õû´óĞ¡
+    resize_X=imresize(X{i},[m,827],'bilinear');%è°ƒæ•´å¤§å°
     for j=1:m
-    crests(6*j-6+1:6*j)=get_crests(resize_X(j,:));%¼ÆËã²¨·å
+    crests(6*j-6+1:6*j)=get_crests(resize_X(j,:));%è®¡ç®—æ³¢å³°
     end
-    vehicle_height=get_vehicle_height(resize_X);%¼ÆËãµ×ÅÌ¸ß¶È
+    vehicle_height=get_vehicle_height(resize_X);%è®¡ç®—åº•ç›˜é«˜åº¦
     X_F(i,:)=[crests,vehicle_height]; 
 end
 end
 
 function [X,Y]=load_data()
-%ÔØÈë³µÁ¾Êı¾İ¼¯
+%è½½å…¥è½¦è¾†æ•°æ®é›†
 l_bus=57;l_car=105;l_SUV=54;l_truck=100;
 X_bus=cell(1,l_bus);
 X_car=cell(1,l_car);
 X_SUV=cell(1,l_SUV);
 X_truck=cell(1,l_truck);
 for i=1:l_bus
-    X_bus{i}=csvread(strcat('vehicle_data_bus/',string(i-1),'.csv'));%ÔØÈë
+    X_bus{i}=csvread(strcat('vehicle_data_bus/',string(i-1),'.csv'));%è½½å…¥
 end
 for i=1:l_car
-    X_car{i}=csvread(strcat('vehicle_data_car/',string(i-1),'.csv'));%ÔØÈë
+    X_car{i}=csvread(strcat('vehicle_data_car/',string(i-1),'.csv'));%è½½å…¥
 end
 for i=1:l_SUV
-    X_SUV{i}=csvread(strcat('vehicle_data_SUV/',string(i-1),'.csv'));%ÔØÈë
+    X_SUV{i}=csvread(strcat('vehicle_data_SUV/',string(i-1),'.csv'));%è½½å…¥
 end
 for i=1:l_truck
-    X_truck{i}=csvread(strcat('vehicle_data_truck/',string(i-1),'.csv'));%ÔØÈë
+    X_truck{i}=csvread(strcat('vehicle_data_truck/',string(i-1),'.csv'));%è½½å…¥
 end
-X=[X_car X_SUV X_bus X_truck];%³µÁ¾Êı¾İ¼¯
-Y=[0*ones(1,l_car) 1*ones(1,l_SUV) 2*ones(1,l_bus) 3*ones(1,l_truck)];%±êÇ©
+X=[X_car X_SUV X_bus X_truck];%è½¦è¾†æ•°æ®é›†
+Y=[0*ones(1,l_car) 1*ones(1,l_SUV) 2*ones(1,l_bus) 3*ones(1,l_truck)];%æ ‡ç­¾
 end
